07bea99099c080720014052160233bb8dce00aee
ziadsamir-CMP <ziadsaimer@gmail.com>
2026-04-17 17:49:32 +0200

    Update GenericDefs.h

--- DS22/DS22/GenericDefs.h
+++ DS22/DS22/GenericDefs.h
@@ -1,6 +1,18 @@
 #pragma once
-enum ORD_TYP
-{
-	
 
+enum ORD_TYPE {
+    ODG, ODN,   // Dine-in
+    OT,         // Takeaway
+    OVC, OVG, OVN // Delivery
+};
+
+enum CHEF_TYPE {
+    CN, // Normal Chef
+    CS  // Special Chef
+};
+enum Status {
+	AVAILABLE,
+	IN_SERVICE,
+	IN_MAINTENANCE,
+	BACK
 };
